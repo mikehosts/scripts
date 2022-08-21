a6ed3931a1d720e3264f2ddc379020686d53532e
mikehosts <111231535+mikehosts@users.noreply.github.com>
2022-08-21 17:57:33 +0200

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,13 +1,13 @@
 echo "What do you want the swapfile to be named?"
 read swapfile
-echo "How many gigabytes do you want the swap to be?"
+echo "How many gigabytes do you want the swap to be? MAKE SURE TO ADD A G AFTER IT!!!"
 read gbs
 echo "Are you sure?, if not, please cntrl + c right now!"
 
-sudo fallocate -l $gbsG /$swapfile
-sudo chmod 600 /$swapfile
-sudo mkswap /$swapfile
-sudo swapon /$swapfile
+fallocate -l $gbs /$swapfile
+chmod 600 /$swapfile
+mkswap /$swapfile
+swapon /$swapfile
 echo '/$swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 echo "DONE"
